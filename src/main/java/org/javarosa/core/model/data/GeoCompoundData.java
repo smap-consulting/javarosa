1dbb099518c157ce9f3675d0b1d19f19bec7ca62
Neil Penman <neilpenman@gmail.com>
2022-09-06 14:49:37 +1000

    Modify geocompound type to be a string, fieldTask will handle the compound data

--- src/main/java/org/javarosa/core/model/data/GeoCompoundData.java
+++ src/main/java/org/javarosa/core/model/data/GeoCompoundData.java
@@ -25,39 +25,24 @@ import org.javarosa.xpath.IExprDataType;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 
 /**
- * A response to a question requesting an GeoTrace Value.
- * Consisting of a comma-separated ordered list of GeoPoint values.
+ * A response to a question requesting an GeoCompound Value.
  *
- * GeoTrace data is an open sequence of geo-locations.
- * GeoShape data is a closed sequence of geo-locations.
- *
- * @author mitchellsundt@gmail.com
+ * @author neilpenman@smap.com.au
  *
  */
 public class GeoCompoundData implements IAnswerData, IExprDataType {
 
     /**
-     * The data value contained in a GeoTraceData object is a GeoTrace
+     * GeoCompoundData is managed as a String
      *
-     * @author mitchellsundt@gmail.com
+     * @author neilpenman@smap.com.au
      *
      */
-    public static class GeoTrace {
-        public final ArrayList<double[]> points;
 
-        public GeoTrace() {
-            points = new ArrayList<>();
-        }
 
-        public GeoTrace(ArrayList<double[]> points) {
-            this.points = points;
-        }
-    }
-
-    public final ArrayList<GeoPointData> points = new ArrayList<>();
+    private String theData = "";
 
 
     /**
@@ -74,16 +59,12 @@ public class GeoCompoundData implements IAnswerData, IExprDataType {
      * @param data
      */
     public GeoCompoundData(GeoCompoundData data) {
-        for ( GeoPointData p : data.points ) {
-            points.add(new GeoPointData(p));
-        }
+        theData = data.theData;
     }
 
-    public GeoCompoundData(GeoTrace atrace) {
-        for ( double[] da : atrace.points ) {
-            points.add(new GeoPointData(da));
-        }
-   }
+    public GeoCompoundData(String data) {
+        theData = data;
+    }
 
     @Override
     public IAnswerData clone() {
@@ -97,26 +78,13 @@ public class GeoCompoundData implements IAnswerData, IExprDataType {
      */
     @Override
     public String getDisplayText() {
-        StringBuilder b = new StringBuilder();
-        boolean first = true;
-        for ( GeoPointData p : points ) {
-            if ( !first ) {
-                b.append("; ");
-            }
-            first = false;
-            b.append(p.getDisplayText());
-        }
-        return b.toString();
+        return theData;
     }
 
 
     @Override
     public Object getValue() {
-        ArrayList<double[]> pts = new ArrayList<>();
-        for ( GeoPointData p : points ) {
-            pts.add((double[])p.getValue());
-        }
-        return new GeoTrace(pts);
+        return theData;
     }
 
 
@@ -125,39 +93,19 @@ public class GeoCompoundData implements IAnswerData, IExprDataType {
         if (o == null) {
             throw new NullPointerException("Attempt to set an IAnswerData class to null.");
         }
-        if ( !(o instanceof GeoTrace) ) {
-            GeoCompoundData t = new GeoCompoundData();
-            GeoCompoundData v = t.cast(new UncastData(o.toString()));
-            o = v.getValue();
-        }
-        GeoTrace gs = (GeoTrace) o;
-        ArrayList<GeoPointData> temp = new ArrayList<>();
-        for ( double[] da : gs.points ) {
-            temp.add(new GeoPointData(da));
-        }
-        points.clear();
-        points.addAll(temp);
+        theData = (String)o;
     }
 
 
     @Override
     public void readExternal(DataInputStream in, PrototypeFactory pf) throws IOException {
-        points.clear();
-        int len = (int) ExtUtil.readNumeric(in);
-        for ( int i = 0 ; i < len ; ++i ) {
-            GeoPointData t = new GeoPointData();
-            t.readExternal(in, pf);
-            points.add(t);
-        }
+        theData = ExtUtil.readString(in);
     }
 
 
     @Override
     public void writeExternal(DataOutputStream out) throws IOException {
-        ExtUtil.writeNumeric(out, points.size());
-        for (GeoPointData t : points) {
-            t.writeExternal(out);
-        }
+        ExtUtil.writeString(out, theData);
     }
 
 
@@ -168,38 +116,19 @@ public class GeoCompoundData implements IAnswerData, IExprDataType {
 
     @Override
     public GeoCompoundData cast(UncastData data) throws IllegalArgumentException {
-        String[] parts = data.value.split(";");
-
-        // silly...
-        GeoPointData t = new GeoPointData();
-
-        GeoCompoundData d = new GeoCompoundData();
-        for ( String part : parts ) {
-            // allow for arbitrary surrounding whitespace
-            d.points.add(t.cast(new UncastData(part.trim())));
-        }
-        return d;
+        return new GeoCompoundData(data.value);
     }
 
 
     @Override
     public Boolean toBoolean() {
         // return whether or not any Geopoints have been set
-        return !points.isEmpty();
+        return !(theData == null || theData.length() == 0);
     }
 
     @Override
     public Double toNumeric() {
-        if ( points.size() == 0 ) {
-            // we have no trace, so no accuracy...
-            return GeoPointData.NO_ACCURACY_VALUE;
-        }
-        // return the worst accuracy...
-        double maxValue = 0.0;
-        for ( GeoPointData p : points ) {
-            maxValue = Math.max(maxValue, p.toNumeric());
-        }
-        return maxValue;
+        return 0.0;
     }
 
     @Override
